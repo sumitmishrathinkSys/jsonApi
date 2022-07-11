af94b030a179c63f70e7d10cbff5fed6141170de
Sumit Mishra <mishra.sumit@thinksys.com>
2022-07-11 11:11:16 +0530

    initial commit

---
+++ posts.js
@@ -0,0 +1,281 @@
+module.exports = {
+    "data": [
+        {
+            "sessionId": "01G793E2D1ZJS2XGRHEWC71VF6",
+            "matchDate": "2022-04-16T17:00:59.000Z",
+            "sessionCreatedAt": "2022-07-06T06:33:58.473Z",
+            "opponentLogo": "https://api.staging.track160.com/api/v0/data/account-01FV01SA2TFRZVZSAMAB44RY4N/media/25d1b7b7-2896-48a7-8dce-20aa66f7776c",
+            "myLogo": "https://api.staging.track160.com/api/v0/data/account-01FV01SA2TFRZVZSAMAB44RY4N/media/fc73b6c8-2256-4867-82a6-f82f46f23842",
+            "highlights": {
+                "teamMetrics": {
+                    "possession": "57:43",
+                    "dangerousSituation": "33:7",
+                    "dominance": "80:20",
+                    "totalDistance": "108.3:105.3",
+                    "corners": "0:0"
+                }
+            }
+        },
+        {
+            "sessionId": "01FZB85P19ZR3KDM3A964ZD6GD",
+            "matchDate": "2022-03-09T10:00:00Z",
+            "sessionCreatedAt": "2022-03-29T16:29:41.669Z",
+            "opponentLogo": "",
+            "myLogo": "https://api.staging.track160.com/api/v0/data/account-01FV01SA2TFRZVZSAMAB44RY4N/media/fc73b6c8-2256-4867-82a6-f82f46f23842",
+            "highlights": {
+                "teamMetrics": {
+                    "possession": "66:34",
+                    "dangerousSituation": "26:9",
+                    "dominance": "74:26",
+                    "totalDistance": "106.8:109.1",
+                    "corners": "4:2"
+                }
+            }
+        },
+        {
+            "sessionId": "01G79PT1SQXEV0QHAYMYN987BQ",
+            "matchDate": "2022-02-20T22:00:00.000Z",
+            "sessionCreatedAt": "2022-07-06T12:12:33.741Z",
+            "opponentLogo": "",
+            "myLogo": "https://api.staging.track160.com/api/v0/data/account-01FV01SA2TFRZVZSAMAB44RY4N/media/fc73b6c8-2256-4867-82a6-f82f46f23842",
+            "highlights": {
+                "teamMetrics": {
+                    "possession": "63:37",
+                    "dangerousSituation": "37:8",
+                    "dominance": "77:23",
+                    "totalDistance": "106.7:108.9",
+                    "corners": "0:0"
+                },
+                "personalMetrics": {
+                    "topSpeedKmh": 25.63,
+                    "totalDistance": 0.51,
+                    "totalMinutesPlayed": 46.77,
+                    "possession": 0
+                }
+            }
+        },
+        {
+            "sessionId": "01G51CP1RE3WSSWMBNCHMG1DHH",
+            "matchDate": "2022-02-07T22:00:00.000Z",
+            "sessionCreatedAt": "2022-06-08T10:10:18.560Z",
+            "opponentLogo": "",
+            "myLogo": "https://api.staging.track160.com/api/v0/data/account-01FV01SA2TFRZVZSAMAB44RY4N/media/fc73b6c8-2256-4867-82a6-f82f46f23842",
+            "highlights": {
+                "teamMetrics": {
+                    "possession": "57:43",
+                    "dangerousSituation": "41:3",
+                    "dominance": "86:14",
+                    "totalDistance": "103.3:93.3",
+                    "corners": "0:0"
+                }
+            }
+        },
+        {
+            "sessionId": "01G518M8YWMMJYD547DNGB0NX3",
+            "matchDate": "2022-02-06T22:01:00.000Z",
+            "sessionCreatedAt": "2022-06-08T08:59:24.186Z",
+            "opponentLogo": "",
+            "myLogo": "https://api.staging.track160.com/api/v0/data/account-01FV01SA2TFRZVZSAMAB44RY4N/media/fc73b6c8-2256-4867-82a6-f82f46f23842",
+            "highlights": {
+                "teamMetrics": {
+                    "possession": "57:43",
+                    "dangerousSituation": "41:3",
+                    "dominance": "86:14",
+                    "totalDistance": "103.3:93.3",
+                    "corners": "0:0"
+                }
+            }
+        },
+        {
+            "sessionId": "01G51KGZB3SKD5CT3YP9S2DKT6",
+            "matchDate": "2022-02-06T22:00:00.000Z",
+            "sessionCreatedAt": "2022-06-08T12:09:56.680Z",
+            "opponentLogo": "https://api.staging.track160.com/api/v0/data/account-01FV01SA2TFRZVZSAMAB44RY4N/media/890ad535-041d-415f-a48b-adf9e9d71ee6",
+            "myLogo": "https://api.staging.track160.com/api/v0/data/account-01FV01SA2TFRZVZSAMAB44RY4N/media/fc73b6c8-2256-4867-82a6-f82f46f23842",
+            "highlights": {
+                "teamMetrics": {
+                    "possession": "55:45",
+                    "dangerousSituation": "43:9",
+                    "dominance": "78:22",
+                    "totalDistance": "108:101.3",
+                    "corners": "0:0"
+                },
+                "personalMetrics": {
+                    "topSpeedKmh": 30.38,
+                    "totalDistance": 2.81,
+                    "totalMinutesPlayed": 45.43,
+                    "possession": 0.2
+                }
+            }
+        },
+        {
+            "sessionId": "01FX24BS5EA03KT3VTHT90FZZE",
+            "matchDate": "2022-01-31T14:00:00Z",
+            "sessionCreatedAt": "2022-03-01T06:58:34.697Z",
+            "opponentLogo": "https://api.staging.track160.com/api/v0/data/account-01FV01SA2TFRZVZSAMAB44RY4N/media/25d1b7b7-2896-48a7-8dce-20aa66f7776c",
+            "myLogo": "https://api.staging.track160.com/api/v0/data/account-01FV01SA2TFRZVZSAMAB44RY4N/media/fc73b6c8-2256-4867-82a6-f82f46f23842",
+            "highlights": {
+                "teamMetrics": {
+                    "possession": "62:38",
+                    "dangerousSituation": "39:14",
+                    "dominance": "71:29",
+                    "totalDistance": "109:106.6",
+                    "corners": "16:3"
+                }
+            }
+        },
+        {
+            "sessionId": "01G79H2N78HTJMYXS6AYFXJYRQ",
+            "matchDate": "2022-01-31T10:00:00Z",
+            "sessionCreatedAt": "2022-07-06T10:32:24.239Z",
+            "opponentLogo": "https://api.staging.track160.com/api/v0/data/account-01FV01SA2TFRZVZSAMAB44RY4N/media/95760c52-a4e9-4196-9141-60d886988dea",
+            "myLogo": "https://api.staging.track160.com/api/v0/data/account-01FV01SA2TFRZVZSAMAB44RY4N/media/fc73b6c8-2256-4867-82a6-f82f46f23842",
+            "highlights": {
+                "teamMetrics": {
+                    "possession": "64:36",
+                    "dangerousSituation": "22:12",
+                    "dominance": "64:36",
+                    "totalDistance": "107.4:111.1",
+                    "corners": "0:0"
+                },
+                "personalMetrics": {
+                    "topSpeedKmh": 29.4,
+                    "totalDistance": 1.55,
+                    "totalMinutesPlayed": 54.36,
+                    "possession": 0.1
+                }
+            }
+        },
+        {
+            "sessionId": "01G79YY8C0HBP0RKG366949FKJ",
+            "matchDate": "2022-01-11T22:00:00.000Z",
+            "sessionCreatedAt": "2022-07-06T14:34:42.696Z",
+            "opponentLogo": "https://api.staging.track160.com/api/v0/data/account-01FV01SA2TFRZVZSAMAB44RY4N/media/890ad535-041d-415f-a48b-adf9e9d71ee6",
+            "myLogo": "https://api.staging.track160.com/api/v0/data/account-01FV01SA2TFRZVZSAMAB44RY4N/media/fc73b6c8-2256-4867-82a6-f82f46f23842",
+            "highlights": {
+                "teamMetrics": {
+                    "possession": "55:45",
+                    "dangerousSituation": "43:9",
+                    "dominance": "78:22",
+                    "totalDistance": "108:101.3",
+                    "corners": "0:0"
+                },
+                "personalMetrics": {
+                    "topSpeedKmh": 30.38,
+                    "totalDistance": 2.81,
+                    "totalMinutesPlayed": 45.43,
+                    "possession": 0.2
+                }
+            }
+        },
+        {
+            "sessionId": "01FX800K4WYFV7K3E3NZYKH87S",
+            "matchDate": "2022-01-02T22:00:00.000Z",
+            "sessionCreatedAt": "2022-03-03T13:38:01.572Z",
+            "opponentLogo": "https://api.staging.track160.com/api/v0/data/account-01FV01SA2TFRZVZSAMAB44RY4N/media/4cb53661-2de2-434b-84c0-396ec5d7db1c",
+            "myLogo": "https://api.staging.track160.com/api/v0/data/account-01FV01SA2TFRZVZSAMAB44RY4N/media/fc73b6c8-2256-4867-82a6-f82f46f23842",
+            "highlights": {
+                "teamMetrics": {
+                    "possession": "65:35",
+                    "dangerousSituation": "32:14",
+                    "dominance": "72:28",
+                    "totalDistance": "108.2:109.1",
+                    "corners": "9:3"
+                },
+                "personalMetrics": {
+                    "topSpeedKmh": 30.6,
+                    "totalDistance": 2.04,
+                    "totalMinutesPlayed": 53.05,
+                    "possession": 0
+                }
+            }
+        },
+        {
+            "sessionId": "01FV7HT34CR7YHEZ16QY1P995W",
+            "matchDate": "2021-12-20T22:00:00.000Z",
+            "sessionCreatedAt": "2022-02-06T12:58:21.020Z",
+            "opponentLogo": "https://api.staging.track160.com/api/v0/data/account-01FV01SA2TFRZVZSAMAB44RY4N/media/9fd8b147-dd41-43f9-8e2b-af86fddf3560",
+            "myLogo": "https://api.staging.track160.com/api/v0/data/account-01FV01SA2TFRZVZSAMAB44RY4N/media/fc73b6c8-2256-4867-82a6-f82f46f23842",
+            "highlights": {
+                "teamMetrics": {
+                    "possession": "59:41",
+                    "dangerousSituation": "28:5",
+                    "dominance": "83:17",
+                    "totalDistance": "113.6:117.3",
+                    "corners": "4:1"
+                }
+            }
+        },
+        {
+            "sessionId": "01FV6YMWBR92RC3FB2EKEVS9BR",
+            "matchDate": "2021-12-17T22:00:00.000Z",
+            "sessionCreatedAt": "2022-02-06T07:23:28.611Z",
+            "opponentLogo": "https://api.staging.track160.com/api/v0/data/account-01FV01SA2TFRZVZSAMAB44RY4N/media/95760c52-a4e9-4196-9141-60d886988dea",
+            "myLogo": "https://api.staging.track160.com/api/v0/data/account-01FV01SA2TFRZVZSAMAB44RY4N/media/fc73b6c8-2256-4867-82a6-f82f46f23842",
+            "highlights": {
+                "teamMetrics": {
+                    "possession": "57:43",
+                    "dangerousSituation": "37:10",
+                    "dominance": "80:20",
+                    "totalDistance": "116.1:119.4",
+                    "corners": "7:2"
+                }
+            }
+        },
+        {
+            "sessionId": "01FV02J3WY71H10YKWW14RQP5K",
+            "matchDate": "2021-12-12T22:00:00.000Z",
+            "sessionCreatedAt": "2022-02-03T15:17:11.590Z",
+            "opponentLogo": "https://api.staging.track160.com/api/v0/data/account-01FV01SA2TFRZVZSAMAB44RY4N/media/890ad535-041d-415f-a48b-adf9e9d71ee6",
+            "myLogo": "https://api.staging.track160.com/api/v0/data/account-01FV01SA2TFRZVZSAMAB44RY4N/media/fc73b6c8-2256-4867-82a6-f82f46f23842",
+            "highlights": {
+                "teamMetrics": {
+                    "possession": "58:42",
+                    "dangerousSituation": "31:4",
+                    "dominance": "82:18",
+                    "totalDistance": "108.3:107",
+                    "corners": "7:0"
+                },
+                "personalMetrics": {
+                    "topSpeedKmh": 30.6,
+                    "totalDistance": 1.64,
+                    "totalMinutesPlayed": 51.98,
+                    "possession": 0
+                }
+            }
+        },
+        {
+            "sessionId": "01FXFAD3TYW5674BFWHE42VRZ0",
+            "matchDate": "2021-10-04T19:16:39.827Z",
+            "sessionCreatedAt": "2022-03-06T09:54:16.962Z",
+            "opponentLogo": "",
+            "myLogo": "https://api.staging.track160.com/api/v0/data/account-01FV01SA2TFRZVZSAMAB44RY4N/media/fc73b6c8-2256-4867-82a6-f82f46f23842",
+            "highlights": {
+                "teamMetrics": {
+                    "possession": "57:43",
+                    "dangerousSituation": "21:9",
+                    "dominance": "74:26",
+                    "totalDistance": "111.3:107.8",
+                    "corners": "7:3"
+                }
+            }
+        },
+        {
+            "sessionId": "01G3R4MR00P4CYHR7T6KPRE8J9",
+            "matchDate": "2021-08-04T21:00:00.000Z",
+            "sessionCreatedAt": "2022-05-23T09:40:56.505Z",
+            "opponentLogo": "",
+            "myLogo": "https://api.staging.track160.com/api/v0/data/account-01FV01SA2TFRZVZSAMAB44RY4N/media/fc73b6c8-2256-4867-82a6-f82f46f23842",
+            "highlights": {
+                "teamMetrics": {
+                    "possession": "64:36",
+                    "dangerousSituation": "39:8",
+                    "dominance": "84:16",
+                    "totalDistance": "107.9:98.1",
+                    "corners": "6:0"
+                }
+            }
+        }
+    ],
+    "success": true
+}
